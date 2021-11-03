2fcd5995715b92df06f62b54408186c4a4a5de6b
William Tulloch <william.tulloch@purple.telstra.com>
2021-11-03 19:10:12 +1100

    Added function to return count of unique id addresses

---
+++ LogParser/LogQuery.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogParser
+{
+    public class LogQuery
+    {
+
+        public int GetNumberOfUniqueIpAddress(List<LogEntry> logEntries)
+        {
+            return logEntries.Distinct(new IpAddressComparer()).Count();
+        }
+    }
+
+    internal class IpAddressComparer : IEqualityComparer<LogEntry>
+    {
+        public bool Equals(LogEntry? x, LogEntry? y)
+        {
+            if( x == null && y == null)
+            {
+                return false;
+            } 
+            if( x == null || y == null )
+            {
+                return false;
+            }
+
+            return x.IpAddress.Equals(y.IpAddress);
+        }
+
+        public int GetHashCode([DisallowNull] LogEntry obj)
+        {
+           return obj.IpAddress.GetHashCode();
+        }
+    }
+}
---
+++ LogParserTests/LogQueryTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogParser;
+using Xunit;
+using Shouldly;
+using System.Net;
+
+namespace LogParserTests
+{
+    public class LogQueryTests
+    {
+        private LogQuery _logQuery;
+
+        public LogQueryTests()
+        {
+            _logQuery = new LogQuery();
+        }
+
+        [Fact]
+        public void GetNumberOfUniqueIpAddress_Given3LogEntries_With2UniqueIpAddresses_ReturnsTheValue2()
+        {
+            var logEntries = new List<LogEntry> { CreateRequestInfo("127.1.120.125", DateTime.Now,"/home/"),
+                 CreateRequestInfo("127.1.120.125", DateTime.Now,"/home/personal/"),
+                  CreateRequestInfo("182.1.200.101", DateTime.Now,"/home/")
+            };
+
+            var result = _logQuery.GetNumberOfUniqueIpAddress(logEntries);
+
+            result.ShouldBe(2);
+        }
+
+        private LogEntry CreateRequestInfo(string ipAddress, DateTime timetamp, string resourcePath)
+        {
+            return new LogEntry
+            {
+                IpAddress = IPAddress.Parse(ipAddress),
+                Timestamp = timetamp,
+                HttpStatusCode = 200,
+                HttpVerb = "GET",
+                Resource = resourcePath
+            };
+        }
+    }
+}
