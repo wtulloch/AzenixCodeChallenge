0ad3742d3f56a95a0b14ef451830e246aab1b5c1
William Tulloch <william.tulloch@purple.telstra.com>
2021-11-04 23:23:52 +1100

    started on console app

---
+++ ParserConsole/FileLogSource.cs
@@ -0,0 +1,51 @@
+﻿using LogParser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserConsole
+{
+    internal class FileLogSource : ILogSource
+    {
+        private string _filePath;
+
+        public FileLogSource(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public List<string> ReadLogs()
+        {
+
+            try
+            {
+                using var stream = File.OpenRead(_filePath);
+
+                using var reader = new StreamReader(stream);
+                string? currentLine;
+                var logs = new List<string>();
+
+                while ((currentLine = reader.ReadLine()) != null)
+                {
+                    logs.Add(currentLine);
+                }
+
+                var line = reader.ReadLine();
+
+                return logs;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
+        }
+
+
+    }
+}
+
--- ParserConsole/Program.cs
+++ ParserConsole/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using LogParser;
+using ParserConsole;
+using static  System.Console;
+
+var filePath = "example-data.log";
+
+try
+{
+    var logSource = new FileLogSource(filePath);
+    var logEntries = new WebLogGenerator().GetLogEntries(logSource).Result;
+
+    var logQueries = new LogQuery();
+
+    var numberOfUniqueClientIpAddresses = logQueries.GetNumberOfUniqueClientIpAddresses(logEntries);
+
+    WriteLine($"Unique client IP Addresses is: {numberOfUniqueClientIpAddresses}");
+
+    var topMostVistedUrls = logQueries.GetTopThreeMostVisitedUrls(logEntries);
+    WriteLine();
+    WriteLine("Top 3 most visited URLs");
+    foreach(var url in topMostVistedUrls)
+    {
+        WriteLine($"\t {url}");
+    }
+
+}
+catch (Exception ex)
+{
+
+    WriteLine(ex.Message);
+   
+}
+
+WriteLine("press any key to close");
+ReadLine();
+
+
