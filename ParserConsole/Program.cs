b42a0a5e851f84a304d1fc5299faf9ec7cf2ae49
William Tulloch <william.tulloch@purple.telstra.com>
2021-11-04 23:31:27 +1100

    completed test console

--- ParserConsole/Program.cs
+++ ParserConsole/Program.cs
@@ -4,6 +4,7 @@ using ParserConsole;
 using static  System.Console;
 
 var filePath = "example-data.log";
+var baseUrl = "http://example.com";
 
 try
 {
@@ -11,18 +12,32 @@ try
     var logEntries = new WebLogGenerator().GetLogEntries(logSource).Result;
 
     var logQueries = new LogQuery();
+    
+    WriteLine("Start");
+    WriteLine();
 
     var numberOfUniqueClientIpAddresses = logQueries.GetNumberOfUniqueClientIpAddresses(logEntries);
-
+    
     WriteLine($"Unique client IP Addresses is: {numberOfUniqueClientIpAddresses}");
-
-    var topMostVistedUrls = logQueries.GetTopThreeMostVisitedUrls(logEntries);
     WriteLine();
+
+    var topMostVistedUrls = logQueries.GetTopThreeMostVisitedUrls(logEntries, baseUrl);
+    
     WriteLine("Top 3 most visited URLs");
     foreach(var url in topMostVistedUrls)
     {
-        WriteLine($"\t {url}");
+        WriteLine($"\t {baseUrl}{url}");
     }
+    WriteLine();
+
+    var topThreeClientIpAddresses = logQueries.GetTopThreeMostActiveClientIpAddresses(logEntries);
+    WriteLine("Top 3 Active client IP Addresses");
+    foreach (var ipAddress in topThreeClientIpAddresses)
+    {
+        WriteLine($"\t{ipAddress}");
+    }
+    WriteLine();
+
 
 }
 catch (Exception ex)
